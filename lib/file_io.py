d0425a40ab8b6548acf458812287fd77216d3245
Travis <travis.williams94@gmail.com>
2023-07-05 14:02:57 -0600

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w') as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a') as text_file:
+        text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', encoding='utf-8') as text_file:
+        return text_file.read()
